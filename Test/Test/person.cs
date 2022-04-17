a5b377a8e5b0db825c0aa4b42131fe06af4e37cb
martinsivanov <martingivanov108@gmail.com>
2022-04-17 18:39:02 +0300

    add Name and LastName to person.cs

--- Test/Test/person.cs
+++ Test/Test/person.cs
@@ -8,6 +8,8 @@
 
     internal class person
     {
+        public string Name { get; set; }
+        public string LastName { get; set; }
         public string FullName { get; set; }
         public string City { get; set; }
     }
